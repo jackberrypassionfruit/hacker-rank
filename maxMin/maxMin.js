06f7e458cfb15f4b41d41e64520ea9292a3648e1
jackberrypassionfruit <pashayanjack@gmail.com>
2022-09-14 15:55:45 -0400

    first commit

---
+++ maxMin/maxMin.js
@@ -0,0 +1,12 @@
+/*
+ * Complete the 'maxMin' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY arr
+ */
+
+const maxMin = (k, arr) => {
+  
+}
